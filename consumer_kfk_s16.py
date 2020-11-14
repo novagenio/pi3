9cfcc4c312e37b0cb0b53365dcd7676c6e3eeca2
Leonardo Gamboa <leogamboa@hotmail.com>
2020-11-14 12:10:54 +0100

    Create consumer_kfk_s16.py

---
+++ consumer_kfk_s16.py
@@ -0,0 +1,29 @@
+# Importing the GPIO library to use the GPIO pins of Raspberry pi
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)  # Using BCM numbering
+host = ""
+port = 1        # Raspberry Pi uses port 1 for Bluetooth Communication
+###################################################
+import time
+from adafruit_servokit import ServoKit
+
+# kafka
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('topic-basic-test', bootstrap_servers=['techhublabs.com:9092'],group_id='topic-basic-test')
+
+
+# esto lo utilizamos solo para desactivar el gpo4, para cable colores
+import RPi.GPIO as GPIO
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, False)
+
+#inicializa  8 cancles
+kit = ServoKit(channels=8)
+
+
+
+for msg in consumer:
+        servo = msg.key.decode("utf-8")
+        grados = msg.value.decode("utf-8")
+        print ("Servo:" + msg.key.decode("utf-8") + " Grados: " + msg.value.decode("utf-8"))
+        kit.servo[int(servo)].angle = int(grados)
